8049e5c5a25065767fb9edc5db4ad1892fd4c533
efeozgur <efecuneytozgur@gmail.com>
2020-11-25 11:51:24 +0300

    Initial commit

---
+++ WebApplication1/Controllers/PersonelController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonelController : Controller
+    {
+        EDataEntities db = new EDataEntities();
+
+        // GET: Personel
+        public ActionResult Add()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Add(tblKisi p)
+        {
+            db.tblKisi.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Ana");
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/TaleplistesiController.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TaleplistesiController : Controller
+    {
+        EDataEntities db = new EDataEntities();
+        // GET: Taleplistesi
+        public ActionResult Index()
+        {            
+            return View(db.Talep.ToList());
+        }
+    }
+}
